ce6212b47396cc48de0c09fc402cd0989079e4b0
edodima13 <edodima13@libero.it>
2022-02-09 12:53:41 +0100

    primo passaggio

---
+++ main.js
@@ -0,0 +1,7 @@
+//salvare le variabili delle classi
+const hamburger = document.querySelector(".hamburger-menu")
+
+const openMenu = document.querySelector(".header-right > a")
+
+const closeMenu = document.querySelector(".hamburger-menu .close")
+
