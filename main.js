da178a80ce0e933f456bfafbfe1703fa11884d3c
edodima13 <edodima13@libero.it>
2022-02-09 12:56:47 +0100

    secondo passaggio

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ const openMenu = document.querySelector(".header-right > a")
 
 const closeMenu = document.querySelector(".hamburger-menu .close")
 
+//rendere visibile la lista
+openMenu.addEventListener("click", function(){
+    hamburger.classList.add("active")
+})
+
